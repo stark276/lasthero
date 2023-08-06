61931fa0376c935f0c67d941c673f06e2862d83d
Kaniet Oskonbaev <lmuzz1993@gmail.com>
2023-08-06 15:19:08 -0700

    ability and armor was added

---
+++ armor.py
@@ -0,0 +1,18 @@
+import random
+
+class Armor:
+
+# instance attributes
+ def __init__(self, name, max_block):
+   self.name = name
+   self.max_block = max_block
+
+# instance method
+ def block(self):
+   random_value = random.randint(0, self.max_block)
+   return random_value
+ 
+if __name__ == "__main__":
+  armor = Armor("Debugging Shield", 10)
+  print(armor.name)
+  print(armor.block())
