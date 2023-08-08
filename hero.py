0974909d4e5c2da21badc4cfda97890c4a246792
Kaniet Oskonbaev <lmuzz1993@gmail.com>
2023-08-08 06:53:14 -0700

    add weapon method added

--- hero.py
+++ hero.py
@@ -1,6 +1,7 @@
 import random
 from ability import Ability
 from armor import Armor
+from weapon import Weapon
 
 
 class Hero:
@@ -75,20 +76,20 @@ class Hero:
         # opponent.add_kill(1)
       
       return self.winner
+    
+  def add_weapon(self, weapon):
+    '''Add weapon to self.abilities'''
+    # This method will append the weapon object passed in as an
+    # argument to self.abilities.
+    # This means that self.abilities will be a list of
+    # abilities and weapons.
+    self.abilities.append(weapon)
 
 
 if __name__ == "__main__":
-  # If you run this file from the terminal
-  # this block is executed.
-
-  hero1 = Hero("Wonder Woman")
-  hero2 = Hero("Dumbledore")
-  ability1 = Ability("Super Speed", 300)
-  ability2 = Ability("Super Eyes", 130)
-  ability3 = Ability("Wizard Wand", 80)
-  ability4 = Ability("Wizard Beard", 20)
-  hero1.add_ability(ability1)
-  hero1.add_ability(ability2)
-  hero2.add_ability(ability3)
-  hero2.add_ability(ability4)
-  hero1.fight(hero2)
+    # If you run this file from the terminal
+    # this block is executed.
+    hero = Hero("Wonder Woman")
+    weapon = Weapon("Lasso of Truth", 90)
+    hero.add_weapon(weapon)
+    print(hero.attack())
